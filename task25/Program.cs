2799a27df1c9d5204a1aadd47f16939f34c678f0
Vladimir83 <vovaf@bk.ru>
2022-12-12 13:43:30 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,21 @@
+﻿int ReadNumber(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine() ?? "";
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+void deegree(int num1, int num2)
+{
+    int pr = 1;
+    for (int i = 0; i < num2; i++)
+    {
+        pr *= num1;
+    }
+    Console.WriteLine($"{num1}^{num2} = {pr}");
+}
+
+int a = ReadNumber("Введите число 1:");
+int b = ReadNumber("Введите число 2:");
+deegree(a,b);
