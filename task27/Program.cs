9b7650bf5457d1d4bce3c06139c5f673f43117f2
Vladimir83 <vovaf@bk.ru>
2022-12-12 13:46:21 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿int ReadNumber(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine() ?? "";
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+void Sum(int n)
+{
+    int N = n;
+    int sum = 0;
+    while (n > 0)
+    {
+        int zifra = n % 10;
+        sum += zifra;
+        n = n / 10; 
+    }
+
+    Console.WriteLine($"Сумма цифр числа {N} равна {sum}");
+}
+
+int num = ReadNumber("Введите число:");
+Sum(num);
